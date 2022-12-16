23373038593dd112550308c273af1c8886b3bac3
sarah <sarah.s.alobaid@gmail.com>
2022-12-16 21:38:04 +0300

    changes in the city error massage

--- bikeshare (1).py
+++ bikeshare (1).py
@@ -26,7 +26,7 @@ def get_filters():
             if city in ['chicago','new york city', 'washington']:
                 break
             else:
-                print("wrong city")
+                print("wrong city,city must be chicago,new york city, or washington")
         except:
             print("wrong city")
         
